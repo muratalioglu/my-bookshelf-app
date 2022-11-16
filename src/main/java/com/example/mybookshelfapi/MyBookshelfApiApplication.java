6114bf8fe468f13a4ab8b3ee2c2aa038bf202c80
Murat <mdenizalioglu@gmail.com>
2022-11-16 22:14:44 +0300

    Initialize project

---
+++ src/main/java/com/example/mybookshelfapi/MyBookshelfApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybookshelfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookshelfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookshelfApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mybookshelfapi/MyBookshelfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybookshelfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookshelfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
