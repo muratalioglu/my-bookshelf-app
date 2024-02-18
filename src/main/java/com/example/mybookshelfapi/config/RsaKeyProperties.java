529acf9deeb3a4448a7dbf5f2dac1fddb8231743
Murat <mdenizalioglu@gmail.com>
2024-02-18 08:14:12 +0300

    Use lombok for rsakeyproperties fields

--- src/main/java/com/example/mybookshelfapi/config/RsaKeyProperties.java
+++ src/main/java/com/example/mybookshelfapi/config/RsaKeyProperties.java
@@ -1,5 +1,7 @@
 package com.example.mybookshelfapi.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
@@ -7,27 +9,13 @@ import org.springframework.stereotype.Component;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
+@Setter
+@Getter
 @Component
 @Primary
 @ConfigurationProperties(prefix = "rsa")
 public class RsaKeyProperties {
 
-    private RSAPublicKey publicKey;
-    private RSAPrivateKey privateKey;
-
-    public RSAPublicKey getPublicKey() {
-        return publicKey;
-    }
-
-    public RSAPrivateKey getPrivateKey() {
-        return privateKey;
-    }
-
-    public void setPublicKey(RSAPublicKey publicKey) {
-        this.publicKey = publicKey;
-    }
-
-    public void setPrivateKey(RSAPrivateKey privateKey) {
-        this.privateKey = privateKey;
-    }
+    RSAPublicKey publicKey;
+    RSAPrivateKey privateKey;
 }
