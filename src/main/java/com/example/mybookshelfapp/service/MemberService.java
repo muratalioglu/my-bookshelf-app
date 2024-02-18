43979ccb4d2bc0ca58a05e67c3976383ec42058d
Murat <mdenizalioglu@gmail.com>
2024-02-18 13:05:39 +0300

    Stop using the unnecessary interfaces

--- src/main/java/com/example/mybookshelfapp/service/MemberService.java
+++ src/main/java/com/example/mybookshelfapp/service/MemberService.java
@@ -1,9 +1,8 @@
 package com.example.mybookshelfapp.service;
 
 import com.example.mybookshelfapp.entity.Member;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface MemberService extends UserDetailsService {
+public interface MemberService  {
 
     Member getMember(String email);
 }
--- src/main/java/com/example/mybookshelfapp/service/MemberServiceImpl.java
+++ src/main/java/com/example/mybookshelfapp/service/MemberServiceImpl.java
@@ -2,8 +2,6 @@ package com.example.mybookshelfapp.service;
 
 import com.example.mybookshelfapp.entity.Member;
 import com.example.mybookshelfapp.repository.MemberRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,10 +16,5 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Member getMember(String email) {
         return memberRepository.findFirstByEmail(email).orElse(null);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return getMember(username);
     }
 }
