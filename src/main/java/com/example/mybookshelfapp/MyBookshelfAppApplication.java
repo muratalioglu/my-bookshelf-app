d948f30bf44466bc21daf30d1b1db9b4d6d71db1
Murat <mdenizalioglu@gmail.com>
2024-02-24 11:38:03 +0300

    Move the initial admin user creation to the CommandLineRunner

--- src/main/java/com/example/mybookshelfapp/MyBookshelfAppApplication.java
+++ src/main/java/com/example/mybookshelfapp/MyBookshelfAppApplication.java
@@ -1,45 +1,15 @@
 package com.example.mybookshelfapp;
 
 import com.example.mybookshelfapp.config.RsaKeyProperties;
-import com.example.mybookshelfapp.entity.MemberRole;
-import com.example.mybookshelfapp.entity.Member;
-import com.example.mybookshelfapp.enums.RoleType;
-import com.example.mybookshelfapp.repository.MemberRepository;
-import com.example.mybookshelfapp.repository.MemberRoleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.Set;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
 public class MyBookshelfAppApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(MyBookshelfAppApplication.class, args);
-
-		MemberRoleRepository roleRepository = context.getBean(MemberRoleRepository.class);
-		MemberRepository memberRepository = context.getBean(MemberRepository.class);
-		BCryptPasswordEncoder bCryptPasswordEncoder = context.getBean(BCryptPasswordEncoder.class);
-
-		Member member = new Member();
-		member.setEmail("admin@mybookshelf");
-		member.setFirstName("Murat Deniz");
-		member.setLastName("Alioglu");
-		member.setRegistrationTime(Timestamp.valueOf(LocalDateTime.of(2023, Month.JANUARY, 1, 0, 0)));
-		member.setPassword(bCryptPasswordEncoder.encode("P4ssw0rd"));
-		memberRepository.save(member);
-
-		roleRepository.saveAll(
-				Set.of(
-						new MemberRole(member.getId(), RoleType.ADMIN.getValue())
-				)
-		);
+		SpringApplication.run(MyBookshelfAppApplication.class, args);
 	}
 }
--- src/main/java/com/example/mybookshelfapp/service/DemoDataService.java
+++ src/main/java/com/example/mybookshelfapp/service/DemoDataService.java
@@ -1,19 +1,27 @@
 package com.example.mybookshelfapp.service;
 
 import com.example.mybookshelfapp.entity.Book;
+import com.example.mybookshelfapp.entity.Member;
 import com.example.mybookshelfapp.entity.MemberBook;
+import com.example.mybookshelfapp.entity.MemberRole;
+import com.example.mybookshelfapp.enums.RoleType;
 import com.example.mybookshelfapp.repository.BookRepository;
 import com.example.mybookshelfapp.repository.MemberBookRepository;
 import com.example.mybookshelfapp.repository.MemberRepository;
+import com.example.mybookshelfapp.repository.MemberRoleRepository;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 
 @Service
@@ -23,6 +31,9 @@ public class DemoDataService implements CommandLineRunner {
     final BookRepository bookRepository;
     final MemberRepository memberRepository;
     final MemberBookRepository memberBookRepository;
+    final MemberRoleRepository memberRoleRepository;
+
+    final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public void run(String... args) {
@@ -62,5 +73,15 @@ public class DemoDataService implements CommandLineRunner {
         }
 
         memberBookRepository.saveAll(memberBookList);
+
+        Member member = new Member();
+        member.setEmail("admin@mybookshelf");
+        member.setFirstName("Murat Deniz");
+        member.setLastName("Alioglu");
+        member.setRegistrationTime(Timestamp.valueOf(LocalDateTime.of(2023, Month.JANUARY, 1, 0, 0)));
+        member.setPassword(bCryptPasswordEncoder.encode("P4ssw0rd"));
+        memberRepository.save(member);
+
+        memberRoleRepository.save(new MemberRole(member.getId(), RoleType.ADMIN.getValue()));
     }
 }
