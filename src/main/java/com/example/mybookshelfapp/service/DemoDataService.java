354352d939c4e28df3e405b51d25da510e30b2ab
Murat <mdenizalioglu@gmail.com>
2024-04-28 20:00:37 +0300

    run db_init.sql file from java code

--- src/main/java/com/example/mybookshelfapp/service/DemoDataService.java
+++ src/main/java/com/example/mybookshelfapp/service/DemoDataService.java
@@ -5,17 +5,18 @@ import com.example.mybookshelfapp.repository.MemberBookRepository;
 import com.example.mybookshelfapp.repository.MemberRepository;
 import com.example.mybookshelfapp.repository.MemberRoleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 @Service
 @RequiredArgsConstructor
-public class DemoDataService {
+public class DemoDataService implements ApplicationRunner {
 
     final BookRepository bookRepository;
     final MemberRepository memberRepository;
@@ -25,8 +26,9 @@ public class DemoDataService {
     final BCryptPasswordEncoder bCryptPasswordEncoder;
     final DataSource dataSource;
 
-    @PostConstruct
-    public void init() {
+    // todo: add conditional run
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.setScripts(new ClassPathResource("db_init.sql"));
         populator.execute(dataSource);
